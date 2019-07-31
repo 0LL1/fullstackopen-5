8952a55f671c3552d00276e04f7b111c1ce621bd
0LL1 <o.a.h.rautiainen@gmail.com>
2019-07-31 19:47:23 +0300

    5.6

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  return (
+    <div className="blog">
+      <p onClick={toggleDetails} className="title">
+        {blog.title} by {blog.author}
+      </p>
+      {showDetails && (
+        <>
+          <p>
+            {blog.likes} likes
+            <button>like</button>
+          </p>
+          <p>Added by {blog.user.name}</p>
+        </>
+      )}
+    </div>
+  )
+}
 
 export default Blog
