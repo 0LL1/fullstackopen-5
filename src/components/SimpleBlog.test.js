c649f59475a85cfd26c83be28e406c236f4c4406
0LL1 <o.a.h.rautiainen@gmail.com>
2019-08-01 19:09:30 +0300

    5.14

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, cleanup } from '@testing-library/react'
+import { render, fireEvent, cleanup } from '@testing-library/react'
 import SimpleBlog from './SimpleBlog'
 
 afterEach(cleanup)
@@ -12,17 +12,30 @@ test('renders title, author, and likes', () => {
     likes: 4
   }
 
-  const onClick = () => {
-    //...
-  }
-
-  const component = render(<SimpleBlog blog={blog} onClick={onClick} />)
-
-  component.debug()
+  const component = render(<SimpleBlog blog={blog} />)
 
   const titleAndAuthor = component.getByText('Test title Test author')
   expect(titleAndAuthor).toBeDefined()
 
   const likes = component.getByText('blog has 4 likes')
   expect(likes).toBeDefined()
+})
+
+test('clicking the like button twice calls the event handler twice', async () => {
+  const blog = {
+    title: 'Test title2',
+    author: 'Test author2',
+    likes: 44
+  }
+
+  const mockHandler = jest.fn()
+
+  const component = render(<SimpleBlog blog={blog} onClick={mockHandler} />)
+
+  const button = component.getByText('like')
+
+  fireEvent.click(button)
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls.length).toBe(2)
 })
